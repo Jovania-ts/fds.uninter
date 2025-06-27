ba93d00960a2d0a0bba9ac780f41c19f576b9630
Jovania Teixeira sampaio <jovana.iurd@gmail.com>
2025-06-26 21:06:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+let botao = document.querySelector("#botao");
+
+botao.addEventListener("mouseover", e =>{
+    botao.style.background="rgb(104, 104, 202)";
+});
+
+botao.addEventListener("mouseout", e =>{
+    botao.style.background="white";
+});
+
+
+botao.addEventListener("click", e =>{
+    botao.style.background="rgb(104, 104, 202)";
+    botao.innerHTML ="Não desista, continue tentando acredite nos seus sonhos";
+});
 
